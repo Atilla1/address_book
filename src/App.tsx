e28d4f7d00c37bc6ab24570527d0a749cbda3da6
atilla <atilla1993@hotmail.se>
2024-07-08 14:02:51 +0200

    finished usercards

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,22 @@ export default function App() {
   console.log(users);
 
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <div className="m-10">
+      <h1 className="text-3xl">Användare</h1>
+      <ul className="grid grid-cols-1 2xl:grid-cols-4 gap-8 justify-items-center mt-4">
+        {users.map((user) => (
+          <li className="w-full p-4 shadow rounded" key={user.id}>
+            <div className="flex">
+              <img className="w-20 rounded-full mr-4" src={user.avatar} />
+              <div className="flex flex-col">
+                <p className="font-semibold">{user.name}</p>
+                <p className="text-gray-500">{user.email}</p>
+                <p className="text-gray-500">{user.phone}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
