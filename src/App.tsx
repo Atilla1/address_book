ff136f06d2ff5a0e8f445a0ca59f18313c70e0d6
atilla <atilla1993@hotmail.se>
2024-07-08 16:02:47 +0200

    added sorting and search

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,46 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getUsers } from "./services/userService";
 import { User } from "./types";
+import _ from "lodash";
+
+type SortOrder = "asc" | "desc";
 
 export default function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortOrder, setSortOder] = useState<SortOrder>("asc");
 
   useEffect(() => {
     getUsers().then((users: any) => setUsers(users));
   }, []);
 
+  const filteredUsers = searchQuery
+    ? users.filter((u) =>
+        u.name.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    : users;
+
+  const sortedUsers = _.orderBy(filteredUsers, "name", sortOrder);
+
   return (
     <div className="m-10">
       <h1 className="text-3xl">Användare</h1>
+      <input
+        className="w-80 mt-2 p-2 border-2 border-neutral-500 rounded "
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <select
+        className="ml-4 mt-2 p-2 border-2 border-neutral-500 rounded"
+        value={sortOrder}
+        onChange={(e) => setSortOder(e.target.value as SortOrder)}
+      >
+        <option value="asc">Stigande</option>
+        <option value="desc">Fallande</option>
+      </select>
       <ul className="grid grid-cols-1 2xl:grid-cols-4 gap-8 justify-items-center mt-4">
-        {users.map((user) => (
+        {sortedUsers.map((user) => (
           <li
             className="w-full p-4 shadow rounded cursor-pointer hover:scale-105 transition-transform duration-300"
             key={user.id}
