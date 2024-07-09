06254028897a9b75879ed0147d8c7cda9390aead
atilla <atilla1993@hotmail.se>
2024-07-09 09:38:16 +0200

    updated api url

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { User } from "../types";
 
-const API_URL = "https://randomuser.me/api/?results=100&seed=atilla&nat=gb";
+const API_URL = "https://randomuser.me/api/?results=28&seed=atilla&nat=gb";
 
 export async function getUsers(): Promise<User[]> {
   const res = await axios.get(API_URL);
